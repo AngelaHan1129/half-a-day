a2192dc93d6b62bafe67545ae998c7900a589623
AngelaHan1129 <tinaowo111@gmail.com>
2026-04-02 00:00:53 +0800

    backend spring boot build

---
+++ backend/halfaday/src/main/java/com/nutc/halfaday/HalfadayApplication.java
@@ -0,0 +1,13 @@
+package com.nutc.halfaday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalfadayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalfadayApplication.class, args);
+	}
+
+}
---
+++ backend/halfaday/src/test/java/com/nutc/halfaday/HalfadayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutc.halfaday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalfadayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
